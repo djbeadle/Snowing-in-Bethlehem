6e04bf57b035f63b403701c0f0d530a51674c9ab
Daniel Beadle <djbeadle@gmail.com>
2017-03-31 19:18:35 +0000

    Fixed a bug that made server claim it was snowing when in reality it was only raining.

--- main.js
+++ main.js
@@ -50,44 +50,48 @@ updateWeather();
 setInterval(updateWeather, config.updaterate);
 
 http.createServer(function(request, response){
-	switch(request.url){
+		switch(request.url){
 		case "/basic.css" :
-			console.log("Got a request for the css");
-			fs.readFile("basic.css", function(error, content){
+		console.log("Got a request for the css");
+		fs.readFile("basic.css", function(error, content){
 				if(error){
-			response.writeHead(200, {"Content-Type": "text/css"});
-					response.writeHead(500);
-					response.end();
+				response.writeHead(200, {"Content-Type": "text/css"});
+				response.writeHead(500);
+				response.end();
 				}
 				else{
-			response.writeHead(200, {"Content-Type": "text/css"});
-					response.end(content, "utf-8");
+				response.writeHead(200, {"Content-Type": "text/css"});
+				response.end(content, "utf-8");
 				}
-			});
+				});
 		break;
 		default:
-			console.log("Got a request for: " + request.url);
-			console.log("User-agent: " + request.headers['user-agent']);
-			console.log("IP: " + request.connection.remoteAddress)
-			console.log("referer: " + request.headers.referer);
-			console.log("remoteAddress: " + request.connection.remoteAddress);
+		console.log("Got a request for: " + request.url);
+		console.log("User-agent: " + request.headers['user-agent']);
+		console.log("IP: " + request.connection.remoteAddress)
+		console.log("referer: " + request.headers.referer);
+		console.log("remoteAddress: " + request.connection.remoteAddress);
 
-			response.writeHead(200, {'Content-Type': 'text/html'});
-			response.write("<head><link rel='stylesheet' href='basic.css'><title>Is it snowing in " + config.placename + "?</title></head>\n");
-			response.write("<h2>IS IT SNOWING IN " + config.placename.toUpperCase() + "?</h2>\n");
-			// response.write(JSON.stringify(current_weather.currently["icon"], 1), "utf8");
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write("<head><link rel='stylesheet' href='basic.css'><title>Is it snowing in " + config.placename + "?</title></head>\n");
+		response.write("<h2>IS IT SNOWING IN " + config.placename.toUpperCase() + "?</h2>\n");
+		// response.write(JSON.stringify(current_weather.currently["icon"], 1), "utf8");
 
-			if(typeof current_weather == "undefined"){
+		console.log("***midnight debugging: ***");
+		console.log(current_weather.currently['precipProbability'] < config.threshold);		
+		console.log(config.weather.indexOf(current_weather.currently['precipType']) == -1);
+		console.log("*** end midnight debugging: ***");
+		if(typeof current_weather == "undefined"){
 			response.write("<h2>A: No data from <a href='https://darksky.net/forecast/" + config.lat + "," + config.lon + "/us12/en' target='_blank'>DarkSky.net</a> try again later.</h2>");
 			response.end("");
-			}
-			else if(current_weather.currently["precipProbability"] < config.threshold){
+		}
+		else if(current_weather.currently["precipProbability"] < config.threshold || config.weather.indexOf(current_weather.currently["precipType"]) == -1){
 			response.write("<body class='no'>\n");
 			response.write("<div><h1>No.</h1></div>\n");
 			response.write("<h3>...Or at least, probably not. This prediction is powered by <a href='https://darksky.net/forecast/" + config.lat + "," + config.lon + "/us12/en' target='_blank'>DarkSky.net</a> and uses their precipitation estimates to determine if it's snowing or not.</h3>");
 			response.end("</body>");
-			}
-			else {
+		}
+		else {
 			// display the percipitation probability for debugging purposes. 
 			/* response.write("Probability of "); 
 			   response.write(JSON.stringify(current_weather.currently["precipType"]));
@@ -95,21 +99,21 @@ http.createServer(function(request, response){
 			   response.write(JSON.stringify(current_weather.currently["precipProbability"]));
 			   response.write(".\n\n"); */
 
-				console.log(current_weather.currently["precipProbability"]);
-				console.log(current_weather.currently["precipType"]);
-				// console.log(current_weather.currently["precipType"] == "snow");
-				//console.log(current_weather.currently["precipProbability"] >= config.threshold && (current_weather.currently["precipType"] == "snow" || current_weather.currently["precipType"] == "sleet"));
-				console.log(current_weather.currently["precipProbability"] >= config.threshold && config.weather.indexOf(current_weather.currently["precipType"]) > -1);
+			console.log(current_weather.currently["precipProbability"]);
+			console.log(current_weather.currently["precipType"]);
+			// console.log(current_weather.currently["precipType"] == "snow");
+			//console.log(current_weather.currently["precipProbability"] >= config.threshold && (current_weather.currently["precipType"] == "snow" || current_weather.currently["precipType"] == "sleet"));
+			console.log(current_weather.currently["precipProbability"] >= config.threshold && config.weather.indexOf(current_weather.currently["precipType"]) > -1);
 
-				if(current_weather.currently["precipProbability"] >= config.threshold && config.weather.indexOf(current_weather.currently["precipType"])){
-					console.log("Holy shit it's snowing!\n");
-					response.write("<body class='yes'>");
-					response.write("<div><h1>YES- COMMENCE PANIC!</h1></div></body>");
-					response.write("<h3>This website is powered by DarkSky.net and uses their precipitation estimates to determine if it's snowing or not.</h3>");
-					response.write("</body>");
-				}
-				response.end("");
+			if(current_weather.currently["precipProbability"] >= config.threshold && config.weather.indexOf(current_weather.currently["precipType"]) > -1){
+				console.log("Holy shit it's snowing!\n");
+				response.write("<body class='yes'>");
+				response.write("<div><h1>YES- COMMENCE PANIC!</h1></div></body>");
+				response.write("<h3>This website is powered by DarkSky.net and uses their precipitation estimates to determine if it's snowing or not.</h3>");
+				response.write("</body>");
 			}
+			response.end("");
+		}
 		break;
 		}
 
